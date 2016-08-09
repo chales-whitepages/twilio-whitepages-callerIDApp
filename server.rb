df22614c311c1faed7cf93ebd067c8db9ed308a6
chales-whitepages <chales@corp.w3data.com>
2016-08-09 12:34:05 -0700

    Iteration

--- server.rb
+++ server.rb
@@ -34,15 +34,14 @@ get '/token' do
   # Get the user-provided ID for the connecting device
   puts "In token"
   device_id = params['device']
-  puts device_id
-  puts "Received token"
+  puts "Received token, " device_if
   # Create a random username for the client
   identity = 'twilioTest'
   # Create a unique ID for the currently connecting device
   endpoint_id = "TwilioDemoApp:#{identity}:#{device_id}"
   # Create an Access Token for the app
-  puts "Created endpoint ID"
-  token = Twilio::Util::AccessToken.new account_sid, api_key, api_secret, 3600, identity
+  puts "Created endpoint ID, " endpoint_id
+  token = Twilio::Util::AccessToken.new account_sid, api_key, api_secret, identity
   # Create app grant for out token
   puts "token created"
   grant = Twilio::Util::AccessToken::SyncGrant.new
