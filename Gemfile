e14fbaff53774497d726d3fc6a7ca8035763cf79
chales-whitepages <chales@corp.w3data.com>
2016-08-09 13:24:36 -0700

    ITeration

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'http://rubygems.org'
+gem 'faker'
 gem 'sinatra'
-gem 'twilio-ruby'
+gem 'sinatra-contrib'
+gemspec :path => 'twilio-ruby'
--- server.rb
+++ server.rb
@@ -1,6 +1,8 @@
+require_relative 'twilio-ruby/lib/twilio-ruby.rb'
 require 'rubygems'
 require 'sinatra'
-require 'twilio-ruby'
+require 'sinatra/json'
+require 'faker'
 
 disable :protection
 
@@ -33,7 +35,6 @@ end
 get '/token' do
   # Get the user-provided ID for the connecting device
   puts "In token"
-  device_id = 'hi'
   device_id = params['device']
   puts device_id
   puts "Received token"
